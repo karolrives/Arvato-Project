7e3970b48fb7d301c608059e9a111513297168df
Carolina <karol.rives@gmail.com>
2019-07-23 17:07:51 -0700

    Added plot_cv_results and plot_learning_curve

--- utils.py
+++ utils.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 import pickle
 
 from sklearn.decomposition import PCA
+from sklearn.model_selection import learning_curve
 
 #Clustering
 from sklearn.cluster import KMeans
@@ -402,3 +403,136 @@ def save_model(model, prefix):
     file = open(prefix + '_model.pkl', 'wb')
     pickle.dump(model, file)
     print(prefix +'_model.pkl saved')
+
+
+def plot_cv_results(df_cv_results, title):
+    '''
+    Plots GridSearchCV results (Training vs Cross Validation)
+
+    :param df_cv_results: GridSearchCv results
+    :param title: title of the plot
+    :return: None
+    '''
+    plt.figure(figsize=(8, 6))
+    plt.title(title,
+              fontsize=16)
+
+    plt.xlabel("Results index")
+    plt.ylabel("Score")
+
+    ax = plt.gca()
+    # ax.set_xlim(0, 402)
+    # ax.set_ylim(0.0, 1)
+
+    # Get the X
+    X_axis = range(1, df_cv_results.shape[0] + 1)
+
+    for sample, color in (('train', 'red'), ('test', 'yellow')):
+        sample_score_mean = df_cv_results['mean_%s_score' % (sample)]
+        sample_score_std = df_cv_results['std_%s_score' % (sample)]
+        ax.fill_between(X_axis, sample_score_mean - sample_score_std,
+                        sample_score_mean + sample_score_std,
+                        alpha=0.1 if sample == 'test' else 0, color=color)
+        ax.plot(X_axis, sample_score_mean, color=color,
+                alpha=1 if sample == 'test' else 0.7,
+                label="ROC AUC (%s)" % (sample), marker='o')
+
+    best_index = np.nonzero(df_cv_results['rank_test_score'] == 1)[0][0]
+    best_score = df_cv_results['mean_test_score'][best_index]
+
+    # Plot a dotted vertical line at the best score for that scorer marked by x
+    ax.plot([X_axis[best_index], ] * 2, [0, best_score],
+            linestyle='-.', color='blue', marker='x', markeredgewidth=3, ms=8)
+
+    # Annotate the best score for that scorer
+    ax.annotate("%0.2f" % best_score, (X_axis[best_index], best_score + 0.005))
+
+    plt.legend(loc="best")
+    plt.grid(True)
+    plt.show()
+
+
+def plot_learning_curve(estimator, title, X, y, ylim=None, cv=5,
+                        n_jobs=-1, train_sizes=np.linspace(.1, 1.0, 5)):
+    """
+    Generate a simple plot of the test and training learning curve.
+    Took it from https://scikit-learn.org/stable/auto_examples/model_selection/plot_learning_curve.html
+    example
+
+    Parameters
+    ----------
+    estimator : object type that implements the "fit" and "predict" methods
+        An object of that type which is cloned for each validation.
+
+    title : string
+        Title for the chart.
+
+    X : array-like, shape (n_samples, n_features)
+        Training vector, where n_samples is the number of samples and
+        n_features is the number of features.
+
+    y : array-like, shape (n_samples) or (n_samples, n_features), optional
+        Target relative to X for classification or regression;
+        None for unsupervised learning.
+
+    ylim : tuple, shape (ymin, ymax), optional
+        Defines minimum and maximum yvalues plotted.
+
+    cv : int, cross-validation generator or an iterable, optional
+        Determines the cross-validation splitting strategy.
+        Possible inputs for cv are:
+          - None, to use the default 3-fold cross-validation,
+          - integer, to specify the number of folds.
+          - :term:`CV splitter`,
+          - An iterable yielding (train, test) splits as arrays of indices.
+
+        For integer/None inputs, if ``y`` is binary or multiclass,
+        :class:`StratifiedKFold` used. If the estimator is not a classifier
+        or if ``y`` is neither binary nor multiclass, :class:`KFold` is used.
+
+        Refer :ref:`User Guide <cross_validation>` for the various
+        cross-validators that can be used here.
+
+    n_jobs : int or None, optional (default=None)
+        Number of jobs to run in parallel.
+        ``None`` means 1 unless in a :obj:`joblib.parallel_backend` context.
+        ``-1`` means using all processors. See :term:`Glossary <n_jobs>`
+        for more details.
+
+    train_sizes : array-like, shape (n_ticks,), dtype float or int
+        Relative or absolute numbers of training examples that will be used to
+        generate the learning curve. If the dtype is float, it is regarded as a
+        fraction of the maximum size of the training set (that is determined
+        by the selected validation method), i.e. it has to be within (0, 1].
+        Otherwise it is interpreted as absolute sizes of the training sets.
+        Note that for classification the number of samples usually have to
+        be big enough to contain at least one sample from each class.
+        (default: np.linspace(0.1, 1.0, 5))
+    """
+    plt.figure()
+    plt.title(title)
+    if ylim is not None:
+        plt.ylim(*ylim)
+    plt.xlabel("Training examples")
+    plt.ylabel("AUC Score")
+    train_sizes, train_scores, test_scores = learning_curve(
+        estimator, X, y, cv=cv, n_jobs=n_jobs, train_sizes=train_sizes, scoring = 'roc_auc')
+    train_scores_mean = np.mean(train_scores, axis=1)
+    train_scores_std = np.std(train_scores, axis=1)
+    test_scores_mean = np.mean(test_scores, axis=1)
+    test_scores_std = np.std(test_scores, axis=1)
+    plt.grid()
+
+    plt.fill_between(train_sizes, train_scores_mean - train_scores_std,
+                     train_scores_mean + train_scores_std, alpha=0.1,
+                     color="r")
+    plt.fill_between(train_sizes, test_scores_mean - test_scores_std,
+                     test_scores_mean + test_scores_std, alpha=0.1, color="g")
+    plt.plot(train_sizes, train_scores_mean, 'o-', color="r",
+             label="Training score")
+    plt.plot(train_sizes, test_scores_mean, 'o-', color="g",
+             label="Cross-validation score")
+
+    plt.legend(loc="best")
+    plt.grid(True)
+    return plt
